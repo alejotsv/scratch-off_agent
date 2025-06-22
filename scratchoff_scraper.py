9f9a2dd8789971f81ec878f9069dc34cbbb5d326
Alejandro Salas <asalas.wd@outlook.com>
2025-06-22 12:00:30 -0400

    Create file with scratchoff links

--- scratchoff_scraper.py
+++ scratchoff_scraper.py
@@ -18,13 +18,18 @@ async def main():
 
         print(f"\n🎯 Found {len(rows)} ticket rows:\n")
 
+        with open("data/scratchoff_links.txt", "w") as f:
+            pass
+
         for row in rows:
             a_tag = await row.query_selector('th a')
             if a_tag:
                 href = await a_tag.get_attribute('href')
                 if href and href.startswith("/"):
                     href = "https://floridalottery.com" + href
-                print(href)
+                with open("data/scratchoff_links.txt", "a") as f:
+                    f.write(href + "\n")
+                print(href + " added to file.")
 
         await browser.close()
 
